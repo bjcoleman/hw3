93593d268daebd7c0a15eb629173d8de6e65fbff
git-keeper <gitkeeper@moravian.edu>
2020-02-05 09:15:05 -0500

    Initial commit

---
+++ src/Help.java
@@ -0,0 +1,9 @@
+public class Help {
+	
+	public Help(){
+		System.out.println("A sample project");
+	}
+	
+	/* ToDo add a new constructor that will print your name and favorite number */
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/*
+Cpourse: CSCI 121 - Computer Science II
+Instructor: Greg Schaper
+Homework 3: Complete the ToDo in the Help class
+Author: <your name here>
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Help help = new Help();
+    }
+    
+}
